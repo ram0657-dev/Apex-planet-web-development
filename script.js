2b73bc91f9318a25ff3600418e00100c64c64b0d
ram0657-dev <ramkrishna1642@gmail.com>
2026-06-14 09:31:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// javascript file for task 1
+// i learned how to do alerts from a youtube video
+
+// this function runs when the button is clicked
+function showAlert() {
+    alert("Hello! Welcome to my first webpage!\n\nThis message was created using JavaScript.\n\nI am learning web development at ApexPlanet Software :)");
+}
